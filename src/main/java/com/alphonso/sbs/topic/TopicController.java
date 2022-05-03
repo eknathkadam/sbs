d324e58d49ac1cdcd436f1fa97399a0d791851dd
eknathkadam <eknathkadam@gmail.com>
2022-05-04 00:41:32 +0530

    added update and delete methods

--- src/main/java/com/alphonso/sbs/topic/TopicController.java
+++ src/main/java/com/alphonso/sbs/topic/TopicController.java
@@ -31,7 +31,18 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void addTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topicService.updateTopic(topic,id);
+	}
 
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+
+	
 }
 
 //GET 		/topics 			Get all topics
