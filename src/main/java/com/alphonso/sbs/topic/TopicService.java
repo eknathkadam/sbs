177a86ff2b2574c32da3aea63b72e3b4e182e599
eknathkadam <eknathkadam@gmail.com>
2022-05-04 12:28:52 +0530

    Added derby database support

--- src/main/java/com/alphonso/sbs/topic/TopicService.java
+++ src/main/java/com/alphonso/sbs/topic/TopicService.java
@@ -1,46 +1,54 @@
 package com.alphonso.sbs.topic;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
+	@Autowired
+	private TopicRepository topicRepository;
 	
-	List<Topic> topics=new ArrayList<>(Arrays.asList(
-			new Topic("spring","Spring framework","Spring Framework Description"),
-			new Topic("java","java framework","java Framework Description"),
-			new Topic("javascript","javascript framework","javascript Framework Description")
-			));
 	
 	public List<Topic>getAllTopics(){
+		List<Topic>topics=new ArrayList<>();
+
+		topicRepository.findAll()
+		.forEach(topics::add);
+		
 		return topics;
+
 	}
 	
 	public Topic getTopic(String id) {
-		
-		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).get();
+		//return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		
+		topicRepository.save(topic);
 	}
 	
 	public void updateTopic(Topic topic, String id) {
-		for(int i=0;i<topics.size();i++) {
-			Topic t = topics.get(i);
-			if (t.getId().equals(id)) {
-				topics.set(i,topic);
-				return;
-			}
-		}
+		
+		topicRepository.save(topic);
+//		for(int i=0;i<topics.size();i++) {
+//			Topic t = topics.get(i);
+//			if (t.getId().equals(id)) {
+//				topics.set(i,topic);
+//				return;
+//			}
+//		}
 	}
 	
 	public void deleteTopic(String id) {
-		topics.removeIf(t->t.getId().equals(id));
+		//topics.removeIf(t->t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 
 }
